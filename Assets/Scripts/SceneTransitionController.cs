347e54fbf8a25447f2fce0284f197b648a51456c
Michal <bananiwo@gmail.com>
2022-12-21 10:14:07 +0100

    add levels

--- Assets/Scripts/SceneTransitionController.cs
+++ Assets/Scripts/SceneTransitionController.cs
@@ -22,25 +22,25 @@ public class SceneTransitionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            if (shouldReveal) shouldReveal = false;
-            else if (!shouldReveal) shouldReveal = true;
-        }
-        if (shouldReveal)
-        {
-            theImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(theImage.material.GetFloat("_Cutoff"), 1.1f, trasitionSpeed * Time.deltaTime));
-        }
-        if (!shouldReveal)
-        {
-            theImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(theImage.material.GetFloat("_Cutoff"), -0.1f - theImage.material.GetFloat("_EdgeSmoothing"), trasitionSpeed * Time.deltaTime));
+    //    if(Input.GetKeyDown(KeyCode.S))
+    //    {
+    //        if (shouldReveal) shouldReveal = false;
+    //        else if (!shouldReveal) shouldReveal = true;
+    //    }
+    //    if (shouldReveal)
+    //    {
+    //        theImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(theImage.material.GetFloat("_Cutoff"), 1.1f, trasitionSpeed * Time.deltaTime));
+    //    }
+    //    if (!shouldReveal)
+    //    {
+    //        theImage.material.SetFloat("_Cutoff", Mathf.MoveTowards(theImage.material.GetFloat("_Cutoff"), -0.1f - theImage.material.GetFloat("_EdgeSmoothing"), trasitionSpeed * Time.deltaTime));
 
-            if(theImage.material.GetFloat("_Cutoff") == -0.1f - theImage.material.GetFloat("_EdgeSmoothing"))
-            {
-                currentLevel.SetActive(false);
-                Instantiate(nextLevel);
-                //nextLevel.SetActive(true);
-            }
-        }
-    }
+    //        if(theImage.material.GetFloat("_Cutoff") == -0.1f - theImage.material.GetFloat("_EdgeSmoothing"))
+    //        {
+    //            currentLevel.SetActive(false);
+    //            Instantiate(nextLevel);
+    //            //nextLevel.SetActive(true);
+    //        }
+    //    }
+    //}
 }
